d00c95cd9c9f21cba004ddee3b04a17109003e21
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-03-22 14:52:51 +0300

    Updates

---
+++ precache-manifest.0bbf2d5d8de8c1ea1ad2663ea13d709e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8622cbf2125f8662d9c5420afd4b647",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "583a19af7349312d0233",
+    "url": "/react-todo-app/static/css/main.8e6e1e0c.chunk.css"
+  },
+  {
+    "revision": "c86804727cacbbecd130",
+    "url": "/react-todo-app/static/js/2.cc04b5fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.cc04b5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583a19af7349312d0233",
+    "url": "/react-todo-app/static/js/main.93111830.chunk.js"
+  },
+  {
+    "revision": "84aed2b3df399991f155",
+    "url": "/react-todo-app/static/js/runtime-main.63a23138.js"
+  }
+]);
